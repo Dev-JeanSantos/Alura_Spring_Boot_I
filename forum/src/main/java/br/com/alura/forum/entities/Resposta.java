dbe0a3b16578e70f1536f63102d6957964ce9635
Dev-JeanSantos <jeancbsan@gmail.com>
2021-02-10 19:10:40 -0300

    Starting with Spring Data JPA

--- forum/src/main/java/br/com/alura/forum/entities/Resposta.java
+++ forum/src/main/java/br/com/alura/forum/entities/Resposta.java
@@ -2,12 +2,24 @@ package br.com.alura.forum.entities;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Resposta {
 	
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String mensagem;
+	
+	@ManyToOne
 	private Topico topico;
 	private LocalDateTime dataCriacao;
+	@ManyToOne
 	private Usuario autor;
 	private Boolean solução = false;
 	
--- forum/src/main/java/br/com/alura/forum/entities/Topico.java
+++ forum/src/main/java/br/com/alura/forum/entities/Topico.java
@@ -4,17 +4,38 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.ManyToOne;
+
+
 import br.com.alura.forum.enums.StatusTopico;
 
+@Entity
 public class Topico {
 	
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String titulo;
 	private String mensagem;
 	private LocalDateTime dataCriacao = LocalDateTime.now();
+	
+	@Enumerated(EnumType.STRING)
 	private StatusTopico status = StatusTopico.NAO_RESPONDIDO;
+	
+	@ManyToOne
 	private Usuario usuario;
+	
+	@ManyToOne
 	private Curso curso;
+	
+	@OneToMany(mappedBy = "topico")
 	private List<Resposta> respostas = new ArrayList<>();
 	
 	public Topico() {
